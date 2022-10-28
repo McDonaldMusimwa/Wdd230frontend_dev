be12f5a5480371c6aed41eedda5a094b3eed4c06
McDonald <104572589+McDonaldMusimwa@users.noreply.github.com>
2022-10-28 16:12:55 +0200

    added second page and changed styling

---
+++ champer/scripts/lazy.js
@@ -0,0 +1,32 @@
+const images = document.querySelectorAll("[data-src]");
+
+
+function preloadImage(ima){
+    const src=ima.getAttribute("data-src");
+    if (!src){
+        return
+    }
+    ima.src=src;
+}
+
+const imageOPtion =()=>{
+
+}
+
+
+const imageObeservor= new IntersectionObserver((entries,imageObeservor)=>{
+
+    entries.forEach(entry=>{
+        if (!entry.isIntersecting){
+            return
+        }else{
+            preloadImage(entry.target)
+            imageObeservor.unobserve(entry.target)
+        }
+    })
+    
+}, imageOPtion);
+
+images.forEach(image=>{
+    imageObeservor.observe(image)
+});
