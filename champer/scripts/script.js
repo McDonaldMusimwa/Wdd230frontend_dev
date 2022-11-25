401c4c76ebc8134cb3ee151f8964e6721f5a69a6
McDonald <104572589+McDonaldMusimwa@users.noreply.github.com>
2022-11-25 13:14:48 +0200

    added contact us

--- champer/scripts/script.js
+++ champer/scripts/script.js
@@ -31,7 +31,7 @@ displaybusiness(daTa)
 
 
 function displaybusiness(businessess) {
- console.log(businessess)
+	console.log(businessess)
 	businessess.forEach(business => {
 		// create the card element and all the elements that will be children of the card
 		let card = document.createElement("section");
@@ -40,6 +40,9 @@ function displaybusiness(businessess) {
 		let image = document.createElement("img");
 		let label = document.createElement("label");
 		let address = document.createElement("p");
+
+		let phone = document.createElement("p");
+
 		let Siteaddress = document.createElement("button");
 		let sitelink = document.createElement("a");
 
@@ -52,15 +55,19 @@ function displaybusiness(businessess) {
 		//set the other none media elements
 		label.textContent = (`${business.name}`);
 		address.textContent = (`${business.address}`);
+		phone.textContent = (`${business.phone}`);
+
 		// the site 
 		//Siteaddress.textContent = ("Visit Us");
-		Siteaddress.setAttribute('class','link')
+		Siteaddress.setAttribute('class', 'link')
 		sitelink.setAttribute('href', business.site);
-		sitelink.textContent="Visit Us";
+		sitelink.textContent = "Visit Us";
 		Siteaddress.appendChild(sitelink);
+
 
 		card.appendChild(image);
 		card.appendChild(label);
+		card.appendChild(phone);
 		card.appendChild(address);
 		card.appendChild(Siteaddress);
 
@@ -71,3 +78,51 @@ function displaybusiness(businessess) {
 
 
 }
+
+spotlightbusiness(daTa);
+
+function spotlightbusiness(businessess) {
+	// create the card element and all the elements that will be children of the card
+	let card1 = document.querySelector(".four");
+
+	//childred of card elemenet
+	let image = document.createElement("img");
+	let label = document.createElement("label");
+	let address = document.createElement("p");
+
+	let phone = document.createElement("p");
+
+	let Siteaddress = document.createElement("button");
+	let sitelink = document.createElement("a");
+
+
+
+	//image of the business
+	image.setAttribute('src', businessess[0].logo);
+	image.setAttribute('alt', businessess[0].name);
+	image.setAttribute('class', 'directory')
+	image.setAttribute('loading', 'lazy');
+
+	//set the other none media elements
+	label.textContent = (`${businessess[0].name}`);
+	address.textContent = (`${businessess[0].address}`);
+	phone.textContent = (`${businessess[0].phone}`);
+
+	// the site 
+	//Siteaddress.textContent = ("Visit Us");
+	Siteaddress.setAttribute('class', 'link')
+	sitelink.setAttribute('href', businessess[0].site);
+	sitelink.textContent = "Visit Us";
+	Siteaddress.appendChild(sitelink);
+
+
+	card1.appendChild(image);
+	card1.appendChild(label);
+	card1.appendChild(phone);
+	card1.appendChild(address);
+	card1.appendChild(Siteaddress);
+
+	//append to the directory element
+	document.querySelector(".rightside").appendChild(card1);
+
+};
